4ddbffdf283e2717d7b4e605a9edb74357641374
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-10 04:05:12 +0600

    database connected and data fetched

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 const { Pool } = require("pg");
+
 require("dotenv").config();
-const connectionString = process.env.PSQL_CONNECTION;
+
 const pool = new Pool({
-  connectionString,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  host: process.env.HOST,
+  port: process.env.PORT,
+  database: process.env.DATABASE,
+  ssl: process.env.DATABASE_SSL === `true`,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
 });
 
-module.exports = {
-  query: (text, params) => pool.query(text, params),
-};
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
+require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+
+const port = 5000;
+
 app.use(express.json());
 app.use(cors());
+
+app.get("/users", async (req, res) => {
+  try {
+    const user = await pool.query("SELECT * FROM employee");
+
+    res.status(201).json({
+      message: `success`,
+      data: user.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`App listening on ${port}`);
