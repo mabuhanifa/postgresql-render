6cf700fa4028da7ea519ac01732f5a2e41f75e22
shourovahmd <moh.abuhanifa@gmail.com>
2023-02-10 22:09:55 +0600

    employee create route

--- server.js
+++ server.js
@@ -21,7 +21,32 @@ app.get("/users", async (req, res) => {
     console.log(error);
   }
 });
+app.post("/users", async (req, res) => {
+  try {
+    const {
+      name,
+      img,
+      email,
+      password,
+      department,
+      leavestatus,
+      isadmin,
+      passreset,
+    } = req.body;
 
+    const newEmployee = await pool.query(
+      "INSERT INTO employee (name, img, email, department, leaveStatus, isAdmin, passReset, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+      [name, img, email, department, leavestatus, isadmin, passreset, password]
+    );
+
+    res.status(201).json({
+      message: `Successfully created a new employee with ${name} and ${email}`,
+      data: newEmployee.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+});
 app.listen(port, () => {
   console.log(`App listening on ${port}`);
 });
